b6969aef26ac3f95fac4a740ca077a09877ee4a4
Alessandro Romano <alessandro.romano@free-now.com>
2022-09-15 22:20:23 +0200

    Add button test function

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function computeScores() {
+  document.getElementById("score1").innerHTML = "Score 1";
+  document.getElementById("score2").innerHTML = "Score 2";
+  document.getElementById("score3").innerHTML = "Score 3";
+  document.getElementById("score4").innerHTML = "Score 4";
+  document.getElementById("score5").innerHTML = "Score 5";
+}
