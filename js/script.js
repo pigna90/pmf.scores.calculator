fdcb784a8787c62eea0f6db491679613d3b57fe6
Alessandro Romano <alessandro.romano@free-now.com>
2022-09-18 11:57:19 +0200

    Retrieve buttons values

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+function returnRadioButtonValue(name) {
+  var ele = document.getElementsByName(name);
+    
+  for(i = 0; i < ele.length; i++) {
+      if(ele[i].checked)
+        return ele[i].name;
+  }
+}
+
 function computeScores() {
   document.getElementById("score1").innerHTML = "Score 1";
   document.getElementById("score2").innerHTML = "Score 2";
   document.getElementById("score3").innerHTML = "Score 3";
   document.getElementById("score4").innerHTML = "Score 4";
-  document.getElementById("score5").innerHTML = "Score 5";
+  document.getElementById("score5").innerHTML = returnRadioButtonValue("mmud")
 }
