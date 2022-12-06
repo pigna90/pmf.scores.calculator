4956b66fa07d6c3473bd22a916d5063d91a2020d
Alessandro Romano <alessandro.romano@free-now.com>
2022-12-06 12:49:55 +0100

    Add allow mising value feature

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function isNumber(n){
+  return n != ""
+}
+
+function isBool(n){
+  return typeof n === 'boolean'
+}
+
 function getRadioButtonStatus(name){
   if (document.getElementById(name + "-yes").checked)
     return true
@@ -9,161 +17,174 @@ function getRadioButtonStatus(name){
 
 function dipss(age, cs, hb, wbc, blasts){
   var score = 0
-  if (age == "" || isNaN(cs) || hb == "" || isNaN(wbc) || blasts == "")
-    return "Can't be calculated (MISSING VALUES)"
+  var missing_values = (age == "" || isNaN(cs) || hb == "" || isNaN(wbc) || blasts == "")
 
-  if (age > 65)
+  if (isNumber(age) && age > 65)
     score += 1
-  if (cs)
+  if (isBool(cs) && cs)
     score += 1
-  if (hb < 10)
+  if (isNumber(hb) && hb < 10)
     score += 2
-  if (wbc)
+  if (isBool(wbc) && wbc)
     score += 1
-  if (blasts >= 1)
+  if (isNumber(blasts) && blasts >= 1)
     score += 1
 
-  if (score == 0)
+  console.log(score)
+  if (score == 0 && !missing_values)
     return "Low(0), median survival NR"
-  else if (score == 1 || score == 2)
+  else if ((score == 1 || score == 2) && !missing_values)
     return "Intermediate-1 (1-2), median survival 14.2 years"
-  else if (score == 3 || score == 4)
+  else if ((score == 3 || score == 4) && !missing_values)
     return "Intermediate-2 (3-4), median survival 4 years"
-  else
+  else if (score > 4 && !missing_values)
     return "High (5-6), median survival 1.5 years"
+  else if (score > 4 && missing_values)
+    return "Warning: can't be calculated for missing values but with available data risk category is: High (5-6), median survival 1.5 years"
+  else
+    return "Can't be calculated (MISSING VALUES)"
 }
 
 function dipss_plus(age, cs, rbc, hb, wbc, plt, blasts, dip){
   var score = 0
-  if (age == "" || isNaN(cs) || isNaN(rbc) || hb == "" || isNaN(wbc) || isNaN(plt) || blasts == "" || isNaN(dip))
-    return "Can't be calculated (MISSING VALUES)"
-    
-  if (age > 65)
+  var missing_values = (age == "" || isNaN(cs) || isNaN(rbc) || hb == "" || isNaN(wbc) || isNaN(plt) || blasts == "" || isNaN(dip))
+
+  if (isNumber(age) && age > 65)
     score += 1
-  if (cs)
+  if (isBool(cs) && cs)
     score += 1
-  if (rbc)
+  if (isBool(rbc) && rbc)
     score += 1
-  if (hb < 10)
+  if (isNumber(hb) && hb < 10)
     score += 1
-  if (wbc)
+  if (isBool(wbc) && wbc)
     score += 1
-  if (plt < 100)
+  if (isNumber(plt) && plt < 100)
     score += 1
-  if (blasts >= 1)
+  if (isNumber(blasts) && blasts >= 1)
     score += 1
-  if (dip)
+  if (isBool(dip) && dip)
     score += 1
 
-  if (score == 0)
+  if (score == 0 && !missing_values)
     return "Low(0), median survival 15.4 years"
-  else if (score == 1)
+  else if (score == 1 && !missing_values)
     return "Intermediate-1 (1), median survival 6.5 years"
-  else if (score == 2 || score == 3)
+  else if ((score == 2 || score == 3) && !missing_values)
     return "Intermediate-2 (2-3), median survival 2.9 years"
+  else if (score > 3 && !missing_values)
+    return "High (>=4), median survival 1.3 years" 
+  else if (score > 3 && missing_values)
+    return "Warning: can't be calculated for missing values but with available data risk category is: High (>=4), median survival 1.3 years"
   else
-    return "High (>=4), median survival 1.3 years"
+    return "Can't be calculated (MISSING VALUES)"
 }
 
 function mipss70(cs, hb, wbc, plt, blasts, type1, hmr, hmr2, bmf){
   var score = 0
-  if (isNaN(cs) || hb == "" || isNaN(wbc) || isNaN(plt) || blasts == "" || isNaN(type1) || isNaN(hmr) || isNaN(hmr2) || isNaN(bmf) || (hmr == true && hmr2 == true))
-    return "Can't be calculated (MISSING VALUES)"
+  var missing_values = (isNaN(cs) || hb == "" || isNaN(wbc) || isNaN(plt) || blasts == "" || isNaN(type1) || isNaN(hmr) || isNaN(hmr2) || isNaN(bmf) || (hmr == true && hmr2 == true))
   
-  if (cs)
+  if (isBool(cs) && cs)
     score += 1
-  if (hb < 10)
+  if (isNumber(hb) && hb < 10)
     score += 2
-  if (wbc)
+  if (isBool(wbc) && wbc)
     score += 2
-  if (plt < 100)
+  if (isNumber(plt) && plt < 100)
     score += 2
-  if (blasts >= 2)
+  if (isNumber(blasts) && blasts >= 2)
     score += 1
-  if (type1)
+  if (isBool(type1) && type1)
     score += 1
-  if (hmr)
+  if (isBool(hmr) && hmr)
     score += 1
-  if (hmr2)
+  if (isBool(hmr2) && hmr2)
     score += 2
-  if (bmf)
+  if (isBool(bmf) && bmf)
     score += 1
   
-  if (score == 0 || score == 1)
+  if ((score == 0 || score == 1) && !missing_values)
     return "Low (0-1), median survival NR"
-  if (score >= 2 && score <= 4)
+  else if ((score >= 2 && score <= 4) && !missing_values)
     return "Intermediate (2-4), median survival 6.3 years"
-  else 
+  else if (score > 4 && !missing_values)
     return "High (≥5), median survival 3.1 years"
-
+  else if (score > 4 && missing_values)
+    return "Warning: can't be calculated for missing values but with available data risk category is: High (≥5), median survival 3.1 years"
+  else
+    return "Can't be calculated (MISSING VALUES)"
 }
 
 function mipss70_plus(cs, hb, female, male, blasts, type1, hmru2, hmr2u2, unmip, vmip){
   var score = 0
-  if (isNaN(cs) || hb == "" || (female == false && male == false) || blasts == "" || isNaN(type1) || isNaN(hmr2u2) || isNaN(hmru2) || (hmru2 == true && hmr2u2 == true) || (unmip == true  && vmip == true) || isNaN(unmip) || isNaN(vmip))
-    return "Can't be calculated (MISSING VALUES)"
+  var missing_values = (isNaN(cs) || hb == "" || (female == false && male == false) || blasts == "" || isNaN(type1) || isNaN(hmr2u2) || isNaN(hmru2) || (hmru2 == true && hmr2u2 == true) || (unmip == true  && vmip == true) || isNaN(unmip) || isNaN(vmip))
   
-  if (cs)
+  if (isBool(cs) && cs)
     score += 2
-  if ((hb < 8 && female) || (hb <9 && male))
+  if (isNumber(hb) && ((hb < 8 && isBool(female) && female) || (hb <9 && isBool(male) && male)))
     score += 2
-  if ((hb>= 8 && hb <= 9.9 && female) || (hb >= 9 && hb <= 10.9 && male))
+  if (isNumber(hb) && ((hb>= 8 && hb <= 9.9 && isBool(female) && female) || (hb >= 9 && hb <= 10.9 && isBool(male) && male)))
     score += 1
-  if (blasts >= 2)
+  if (isNumber(blasts) && blasts >= 2)
     score += 1
-  if (type1)
+  if (isBool(type1) && type1)
     score += 2
-  if (hmru2)
+  if (isBool(hmru2) && hmru2)
     score += 2
-  if (hmr2u2)
+  if (isBool(hmr2u2) && hmr2u2)
     score += 3
-  if (unmip)
+  if (isBool(unmip) && unmip)
     score += 3
-  if (vmip)
+  if (isBool(vmip) && vmip)
     score += 4
 
-  console.log(score)
-
-  if (score == 0)
+  if (score == 0 && !missing_values)
     return "Very low (0), median survival NR"
-  else if (score == 1 || score == 2)
+  else if ((score == 1 || score == 2) && !missing_values)
     return "Low (1-2), median survival 16.4 years"
-  else if (score == 3 || score == 4)
+  else if ((score == 3 || score == 4) && !missing_values)
     return "Intermediate (3-4), median survival 7.7 years"
-  else if (score >= 5 && score <= 8)
+  else if ((score >= 5 && score <= 8) && !missing_values)
     return "High (5-8), median survival 4.1 years"
-  else
+  else if (score > 8 && !missing_values)
     return "Very high (≥9), median survival 1.8 years"
+  else if (score > 8 && missing_values)
+    return "Warning: can't be calculated for missing values but with available data risk category is: Very high (≥9), median survival 1.8 years"
+  else
+    return "Can't be calculated (MISSING VALUES)"
 }
 
 function mtss(age, knf, mmud, wbc, plt, calr, as_){
   var score = 0
-  if (age == "" || isNaN(knf) || isNaN(mmud) || isNaN(wbc) || plt == "" || isNaN(calr) || isNaN(as_))
-    return "Can't be calculated (MISSING VALUES)"
+  var missing_values = (age == "" || isNaN(knf) || isNaN(mmud) || isNaN(wbc) || plt == "" || isNaN(calr) || isNaN(as_))
 
-  if (age >= 57)
+  if (isNumber(age) && age >= 57)
     score += 1
-  if (knf)
+  if (isBool(knf) && knf)
     score += 1
-  if (mmud)
+  if (isBool(mmud) && mmud)
     score += 2
-  if (wbc)
+  if (isBool(wbc) && wbc)
     score += 1
-  if (plt < 150)
+  if (isNumber(plt) && plt < 150)
     score += 1
-  if (calr)
+  if (isBool(calr) && calr)
     score += 2
-  if (as_)
+  if (isBool(as_) && as_)
     score += 1
   
-  if (score >= 0 && score <= 2)
+  if ((score >= 0 && score <= 2) && !missing_values)
     return "Low (0-2), 5-year survival 90%"
-  else if (score == 3 && score == 4)
+  else if ((score == 3 && score == 4) && !missing_values)
     return "Intermediate (3-4), 5-year survival 77%"
-  else if (score == 5)
+  else if (score == 5 && !missing_values)
     return "High (5), 5-year survival 50%"
-  else if (score >= 6 && score <= 9)
+  else if ((score >= 6 && score <= 9) && !missing_values)
     return "Very high (6-9), 5-year survival 34%"
+  else if ((score >= 6 && score <= 9) && missing_values)
+    return "Warning: can't be calculated for missing values but with available data risk category is: Very high (6-9), 5-year survival 34%"
+  else
+    return "Can't be calculated (MISSING VALUES)"
 }
 
 function computeScores() {
